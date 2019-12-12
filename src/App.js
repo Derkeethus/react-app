0e7883b3ea8195fe2e682e47a8437916b1bdd8e0
TJ Foster <A01165467@ad.bcit.ca>
2019-12-12 10:40:29 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Amazon will consume all
         </p>
         <a
           className="App-link"
